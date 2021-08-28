aaca351bcd073908612b095c03136388e3748bbf
mtan22 <76962221+mtan22@users.noreply.github.com>
2021-08-27 18:46:34 -0700

    Create planets_tests.py

---
+++ labs/lab0/planets_tests.py
@@ -0,0 +1,26 @@
+import unittest
+from planets import *
+
+
+class Test_planets(unittest.TestCase):
+    def test_mars(self) -> None:
+        weight = 136
+        self.assertAlmostEqual(weight_on_planets(weight, 'Mars'), 51.68)
+
+    def test_jupiter(self) -> None:
+        weight = 136
+        self.assertAlmostEqual(weight_on_planets(weight, 'Jupiter'), 318.24)
+
+    def test_venus(self) -> None:
+        weight = 136
+        self.assertAlmostEqual(weight_on_planets(weight, 'Venus'), 123.76)
+
+    def test_error(self) -> None:
+
+        with self.assertRaises(ValueError):  # uses context manager for checking exception
+            weight = 99
+            weight_on_planets(weight, "Neptune")
+
+
+if __name__ == "__main__":
+    unittest.main()
